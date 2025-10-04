f00544bafa475abae3ad1a9220b61d6aa71d8745
unknown <ej.perlazac@uea.edu.ec>
2025-10-04 16:15:37 -0300

    📝 Tarea: manejo de archivos de texto en Python (my_notes)

---
+++ my_notes.py
@@ -0,0 +1,29 @@
+# Tarea: Trabajo con Archivos de Texto en Python
+# Universidad Estatal Amazónica
+# Autor: Evelyn Perlaza
+
+# =========================================
+# 1️⃣ ESCRITURA DE ARCHIVO DE TEXTO
+# =========================================
+archivo = open("my_notes.txt", "w")
+
+archivo.write("Aprender Python me ayuda a desarrollar la lógica para resolver problemas.\n")
+archivo.write("La programación me permite automatizar tareas que antes hacía manualmente.\n")
+archivo.write("Comprender cómo funcionan los archivos facilita el manejo de información real.\n")
+
+archivo.close()
+print("Archivo 'my_notes.txt' creado y guardado correctamente.\n")
+
+# =========================================
+# 2️⃣ LECTURA DE ARCHIVO DE TEXTO
+# =========================================
+archivo = open("my_notes.txt", "r")
+
+print("=== Lectura del contenido del archivo 'my_notes.txt' ===\n")
+linea = archivo.readline()
+while linea:
+    print(linea.strip())
+    linea = archivo.readline()
+
+archivo.close()
+print("\nEl archivo 'my_notes.txt' fue cerrado exitosamente.")
